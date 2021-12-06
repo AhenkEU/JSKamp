8f8e9260ccb893459c54ddfeb58e4e9ccc67d05c
harmoonia <ahenk.er@gmail.com>
2021-12-06 18:07:13 +0300

    18 Ekim Ders Sonu

--- src/app.js
+++ src/app.js
@@ -1,36 +1,32 @@
-console.log("Merhaba Kodlama.io")
+console.log("Merhaba kodlama.io")
 
-//JS type safe değildir
+//JS typesafe değildir.
 let dolarBugun = 9.30
-
 let dolarDun = 9.20
-dolarDun = "9.20"
+
+
+
 {
+
     let dolarDun = 9.10
 }
 
 console.log(dolarDun)
 
 const euroDun = 11.2
-//euroDun = 11
+euroDun = 11
 
 console.log(euroDun)
 
-//array
 //camelCasing
 //PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
 
-console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
-    console.log("<li>"+konutKredileri[i]+"</li>")
-}
-console.log("</ul>")
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi","Kamu Konut Kredisi"]
 
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
 
 console.log(konutKredileri)
+
+for (let i=0; i < konutKredileri.length; i++) {
+    console.log( "<li>" +konutKredileri[i]+ "<li>")
+    
+}
